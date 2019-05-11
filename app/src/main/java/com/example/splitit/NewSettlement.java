92cd363dc3a24e9ff0e57670f6c6caa2f0187665
espen0602 <espen.lindstad95@gmail.com>
2019-05-11 16:17:07 -0700

    New settlement

---
+++ app/src/main/java/com/example/splitit/NewSettlement.java
@@ -0,0 +1,13 @@
+package com.example.splitit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewSettlement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_settlement);
+    }
+}
