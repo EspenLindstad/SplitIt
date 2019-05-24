d82fb8a9a183ea43ad7e23d0cf21ce547c23b37a
Espen Lindstad <Espen.lindstad95@gmail.com>
2019-05-24 15:13:10 -0700

    NameGroupPage initiated

--- app/src/main/java/com/example/splitit/AddGroupMember.java
+++ app/src/main/java/com/example/splitit/AddGroupMember.java
@@ -1,25 +1,52 @@
 package com.example.splitit;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 public class AddGroupMember extends AppCompatActivity {
 
-    private Set<Integer> memberList;
+    private ArrayList<String> memberList = new ArrayList<>();
 
 
     private Button nextButton;
     private Button backButton;
-    private Button testButton;
 
     private TextView topText;
+
+    public FirebaseAuth mAuth;
+
+
+    private static final String TAG = "UserList";
+    private DatabaseReference userlistReference;
+    private ValueEventListener mUserListListener;
+    ArrayList<String> usernamelist = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
+    ;
+
+    public ListView userListView;
+
+    public TextView titleTextView;
 
 
 
@@ -28,9 +55,12 @@ public class AddGroupMember extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_group_member);
 
-        final Button nextButton = (Button) findViewById(R.id.nextButton);
+        userlistReference = FirebaseDatabase.getInstance().getReference().child("users");
+        userListView = (ListView) findViewById(R.id.userlistview);
+        onStart();
+
+        nextButton = (Button) findViewById(R.id.nextButton);
         Button backButton = (Button) findViewById(R.id.backButton);
-        Button testButton = (Button) findViewById(R.id.testButton); // Dette skal egentlig være en person
 
         nextButton.setEnabled(false);
 
@@ -50,6 +80,8 @@ public class AddGroupMember extends AppCompatActivity {
             }
         });
 
+        /*
+
         testButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,19 +89,61 @@ public class AddGroupMember extends AppCompatActivity {
                 changeClickability(memberList, nextButton);
             }
         });
+        */
 
 
 
     }
 
-    private void changeClickability(Set set, Button btn) {
-        if (!set.isEmpty()) {
-            btn.setEnabled(true);
+    protected void onStart() {
+        super.onStart();
+        final ValueEventListener userListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    //Log.i(TAG, "user " + snapshot);
+                    Log.d("item id ",snapshot.child("name").getValue().toString());
+                    usernamelist.add(snapshot.child("name").getValue().toString());
+                    //System.out.println(snapshot);
+                }
+                Log.i(TAG, "onDataChange: " + usernamelist.toString());
+                arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, usernamelist);
+                userListView.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "onCancelled: ", databaseError.toException());
+                Toast.makeText(getApplicationContext(), "Failed to load User list.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+        userlistReference.addValueEventListener(userListener);
+
+        mUserListListener = userListener;
+
+        userListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int posID = (int)id;
+
+                if (!memberList.contains(usernamelist.get(position))) {
+                    memberList.add(usernamelist.get(position));
+                }
+                changeClickability();
+                System.out.println(memberList);
+
+            }
+        });
+    }
+
+    private void changeClickability() {
+        if (!memberList.isEmpty()) {
+            nextButton.setEnabled(true);
         }
 
     }
 
-    //Må fikse sharedPreferences
 
 
 }
---
+++ app/src/main/java/com/example/splitit/NameGroupPage.java
@@ -0,0 +1,13 @@
+package com.example.splitit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NameGroupPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_group_page);
+    }
+}
